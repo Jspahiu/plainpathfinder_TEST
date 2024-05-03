56f079aa2e09caffa82dd626f7ba037f60af1bc8
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2024-05-03 18:48:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function button1() {
+    document.location.href = 'limted-capti.html',true;
+}
+
+function button2() {
+    document.location.href = "game_unblocker.html";
+}
+
+function button3() {
+    document.location.href = "updates.html";
+}
+
+function Menu() {
+    document.location.href = "index.html";
+}
+
+function full_screen_button() {
+    document.location.href = "full_screen_game.html";
+}
